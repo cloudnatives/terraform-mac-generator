ef97e36f4081fd02fb607234996fee57ef6de13b
Jeroen Schepens <jeroenschepens@telenet.be>
2020-04-13 20:30:05 +0200

    Use lowercase hex digits

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   bytes_per_mac_address = 6
 
-  hexadecimal_digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
-  unicast_digits     = ["2", "6", "A", "E"]
+  hexadecimal_digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+  unicast_digits     = ["2", "6", "a", "e"]
 
   list_of_bytes = chunklist(random_id.mac_parts.*.hex, local.bytes_per_mac_address - 1)
 
